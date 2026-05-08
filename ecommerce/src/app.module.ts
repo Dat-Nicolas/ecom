47372cfd50fbf0b1a929c0caae48b87bbb28f0fb
Dat-Nicolas <topchit031@gmail.com>
2026-05-09 01:45:43 +0700

    fix 11

--- ecommerce/src/app.module.ts
+++ ecommerce/src/app.module.ts
@@ -1,10 +1,10 @@
-import { CacheModule } from '@nestjs/cache-manager';
+// import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ThrottlerModule, seconds } from '@nestjs/throttler';
-import { redisStore } from 'cache-manager-redis-yet';
+// import { redisStore } from 'cache-manager-redis-yet';
 // import { KafkaModule } from './config/kafka/kafka.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { BrandsModule } from './modules/brands/brands.module';
@@ -19,7 +19,7 @@ import { ReviewsModule } from './modules/reviews/reviews.module';
 import { ShipmentsModule } from './modules/shipments/shipments.module';
 import { UploadModule } from './modules/upload/upload.module';
 import { UsersModule } from './modules/users/users.module';
-// import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -42,25 +42,25 @@ import { UsersModule } from './modules/users/users.module';
       },
     }),
 
-    CacheModule.registerAsync({
-      isGlobal: true,
-      inject: [ConfigService],
-      useFactory: async (cfg: ConfigService) => ({
-        store: await redisStore({
-          socket: {
-            host: cfg.get('REDIS_HOST', 'localhost'),
-            port: cfg.get('REDIS_PORT', 6379),
-          },
-          password: cfg.get('REDIS_PASSWORD') || undefined,
-          ttl: cfg.get('REDIS_TTL', 3600),
-        }),
-      }),
-    }),
+    // CacheModule.registerAsync({
+    //   isGlobal: true,
+    //   inject: [ConfigService],
+    //   useFactory: async (cfg: ConfigService) => ({
+    //     store: await redisStore({
+    //       socket: {
+    //         host: cfg.get('REDIS_HOST', 'localhost'),
+    //         port: cfg.get('REDIS_PORT', 6379),
+    //       },
+    //       password: cfg.get('REDIS_PASSWORD') || undefined,
+    //       ttl: cfg.get('REDIS_TTL', 3600),
+    //     }),
+    //   }),
+    // }),
 
     ScheduleModule.forRoot(),
     EventEmitterModule.forRoot(),
 
-    // PrismaModule,
+    PrismaModule,
     // KafkaModule,
 
     AuthModule,
